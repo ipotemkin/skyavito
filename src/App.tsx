e08368f83bffa51ee03d7457d39f58cef2379bd5
Igor Potemkin <ipotemkin@rambler.ru>
2022-11-10 16:11:53 +0300

    feat: add HeartIcon

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import './App.css'
 import { Button } from './components/Button/Button'
+import HeartIcon from './icons/Heart/HeartIcon'
 
 export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style={{ gap: 16 }}>
         <p>Starting Diploma</p>
         <Button >Сохранить</Button>
         <Button disabled>Disabled</Button>
         <Button type="outlined">Outlined</Button>
         <Button type="secondary" size="m">Сохранить</Button>
+        <HeartIcon />
+        <div style={{ width: 200, height: 100, backgroundColor: "#009EE4" }}>
+          <HeartIcon type="secondary"/>
+        </div>
       </header>
     </div>
   )
