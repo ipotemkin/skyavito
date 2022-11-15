f93ecb7bfa08f9d4d12ca82fdc7188284c03ed07
Igor Potemkin <ipotemkin@rambler.ru>
2022-11-15 17:05:41 +0300

    refactor: SubHeader styles

--- src/pages/Page/Page.tsx
+++ src/pages/Page/Page.tsx
@@ -3,19 +3,22 @@ import { Footer } from '../../components/Footer/Footer'
 import { Header } from '../../components/Header/Header'
 import { Search } from '../../components/Search/Search'
 import { SearchMob } from '../../components/SearchMob/SearchMob'
+import { SubHeader } from '../../components/SubHeader/SubHeader'
 
 import styles from './style.module.css'
 
 type Props = {
   children?: ReactNode
+  mode?: 'search' | 'subheader'
 }
 
-export const Page: FC<Props> = ({ children }) => {
+export const Page: FC<Props> = ({ children, mode = 'search' }) => {
   return (
     <div className={styles.page}>
       <header><Header /></header>
       <div className={styles.desktop}>
-        <Search />
+        {mode === 'search' && <Search />}
+        {mode !== 'search' && <SubHeader />}
       </div>
       <div className={styles.mobile}>
         <SearchMob />
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,22 +1,18 @@
 import React from 'react'
 
 import { EditProfile } from '../../components/EditProfile/EditProfile'
-import { Header } from '../../components/Header/Header'
 import { MyGoods } from '../../components/MyGoods/MyGoods'
-import { SubHeader } from '../../components/SubHeader/SubHeader'
+import { PageTitle } from '../../components/PageTitle/PageTitle'
+import { Page } from '../Page/Page'
 
-import styles from './style.module.css'
+// import styles from './style.module.css'
 
 export const Profile = () => {
   return (
-    <div>
-      <Header />
-      <SubHeader />
-      <div className={styles.container}>
-        <h2 className={styles.h2}>Здравствуйте, !</h2>
-        <EditProfile />
-        <MyGoods />
-      </div>
-    </div>
+    <Page mode="subheader">
+      <PageTitle>Здравствуйте, !</PageTitle>
+      <EditProfile />
+      <MyGoods />
+    </Page>
   )
 }
