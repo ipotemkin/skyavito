51df58ea58e4f4ecef049fba4d8a3ac0c6ffe0a8
Igor Potemkin <ipotemkin@rambler.ru>
2022-11-16 20:45:11 +0300

    feat: add adaptive for SignupModal

--- src/components/AuthForm/SignupModal.tsx
+++ src/components/AuthForm/SignupModal.tsx
@@ -9,6 +9,7 @@ import { Modal } from '../Modal/Modal'
 import { useAppDispatch, useAppSelector } from '../../app/hooks'
 import { hideModals, selectModal } from '../../slices/modalSlice'
 import { ROUTES } from '../../routes'
+import { Page } from '../../pages/Page/Page'
 
 type Props = {
   isOpen?: boolean
@@ -28,39 +29,42 @@ export const SignupModal: FC<Props> = ({ isOpen = false }) => {
 
   return (
     <Modal isModalOpenArg={modalShownName === 'signup'}>
-      <form className={styles.signup} onSubmit={handleSubmit}>
-        <div className={styles.logo}>
-          <LogoIcon />
-        </div>
-        <input className={styles.input}
-          type="text"
-          placeholder="email"
-          autoFocus
-        />
-        <input className={styles.input}
-          type="password"
-          placeholder="Пароль"
-        />
-        <input className={styles.input}
-          type="password"
-          placeholder="Повторите пароль"
-        />
-        <input className={styles.input}
-          type="text"
-          placeholder="Имя (необязательно)"
-        />
-        <input className={styles.input}
-          type="text"
-          placeholder="Фамилия (необязательно)"
-        />
-        <input className={styles.input}
-          type="text"
-          placeholder="Город (необязательно)"
-        />
-        <Button style={{ width: 278, height: 52, marginTop: 30 }}>
-          Зарегистрироваться
-        </Button>
+      <Page mode="mobOnly">
+        <form className={styles.signup} onSubmit={handleSubmit}>
+          <div className={styles.logo}>
+            <LogoIcon />
+          </div>
+          <input className={styles.input}
+            type="text"
+            placeholder="email"
+            autoFocus
+          />
+          <input className={styles.input}
+            type="password"
+            placeholder="Пароль"
+          />
+          <input className={styles.input}
+            type="password"
+            placeholder="Повторите пароль"
+          />
+          <input className={styles.input}
+            type="text"
+            placeholder="Имя (необязательно)"
+          />
+          <input className={styles.input}
+            type="text"
+            placeholder="Фамилия (необязательно)"
+          />
+          <input className={styles.input}
+            type="text"
+            placeholder="Город (необязательно)"
+          />
+          <div className={styles.horDiv}></div>
+          <Button style={{ width: 278, height: 52 }}>
+            Зарегистрироваться
+          </Button>
       </form>
+      </Page>
     </Modal>
   )
 }
