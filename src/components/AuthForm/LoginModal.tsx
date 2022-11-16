a2ec9af778224f6939b8fa59341897d62f3e29c8
Igor Potemkin <ipotemkin@rambler.ru>
2022-11-16 20:33:36 +0300

    feat: add adaptive for LoginModal

--- src/components/AuthForm/LoginModal.tsx
+++ src/components/AuthForm/LoginModal.tsx
@@ -6,6 +6,7 @@ import LogoIcon from '../../icons/Logo/LogoIcon'
 import { Modal } from '../Modal/Modal'
 import { useAppDispatch, useAppSelector } from '../../app/hooks'
 import { selectModal, showModal } from '../../slices/modalSlice'
+import { Page } from '../../pages/Page/Page'
 
 type Props = {
   isOpen?: boolean
@@ -27,28 +28,31 @@ export const LoginModal: FC<Props> = ({ isOpen = false, handleOpen = () => void 
 
   return (
     <Modal isModalOpenArg={modalShownName === 'login'}>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <div className={styles.logo}>
-          <LogoIcon />
-        </div>
-        <input className={styles.input}
-          type="text"
-          placeholder="email"
-          autoFocus
-        />
-        <input className={styles.input}
-          type="password"
-          placeholder="Пароль"
-        />
-        <Button style={{ marginTop: 30, marginBottom: 20, width: 278, height: 52 }}>
-          Войти
-        </Button>
-        <Button type="outlined" style={{ width: 278, height: 52 }}
-          onClick={handleRegister}
-        >
-          Зарегистрироваться
-        </Button>
-      </form>
+      <Page mode="mobOnly">
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <div className={styles.logo}>
+            <LogoIcon />
+          </div>
+          <input className={styles.input}
+            type="text"
+            placeholder="email"
+            autoFocus
+          />
+          <input className={styles.input}
+            type="password"
+            placeholder="Пароль"
+          />
+          <div className={styles.horDiv}></div>
+          <Button style={{ marginBottom: 20, width: 278, height: 52 }}>
+            Войти
+          </Button>
+          <Button type="outlined" style={{ width: 278, height: 52 }}
+            onClick={handleRegister}
+          >
+            Зарегистрироваться
+          </Button>
+        </form>
+      </Page>
     </Modal>
   )
 }
