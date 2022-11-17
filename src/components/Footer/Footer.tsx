42cf4dd2748b9b80684ffa2abada97151253e3ca
Igor Potemkin <ipotemkin@rambler.ru>
2022-11-17 22:18:16 +0300

    refactor: Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+
 import { useAppDispatch } from '../../app/hooks'
 import HomeIcon from '../../icons/Home/HomeIcon'
 import NewIcon from '../../icons/New/NewIcon'
