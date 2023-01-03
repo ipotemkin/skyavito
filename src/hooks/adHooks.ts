ad24c8608042c9d0ce6aa99b033ff3c60d2a5072
Igor Potemkin <ipotemkin@rambler.ru>
2023-01-03 13:55:45 +0600

    refactor: adHooks

--- src/hooks/adHooks.ts
+++ src/hooks/adHooks.ts
@@ -1,25 +1,10 @@
-import { skipToken } from "@reduxjs/toolkit/dist/query"
 import { useEffect, useState } from "react"
-import { useGetAdsByUserIdQuery, useGetProductsQuery } from "../api/products.api"
+
+import { useGetProductsQuery } from "../api/products.api"
 import { CardType } from "../types"
 
-// export const useAdsFiltered = (filter = '') => {
-//   const { data, isLoading, error } = useGetProductsQuery()
-//   const [filteredData, setFilteredData] = useState<CardType[]>([])
-
-//   useEffect(() => {
-//     if (data) {
-//       const newData = data.filter((item: CardType) => 
-//         item.title.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
-//       )
-//       setFilteredData(newData)
-//     }
-//   }, [data, filter])
-
-//   return { data: filteredData || data, isLoading, error }
-// }
-
 export const useAdsFiltered = (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   filter = '', queryHook: any = useGetProductsQuery, id?: number
   ) => {
   const { data, isLoading, error } = queryHook(id)
