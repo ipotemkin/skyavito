3ecd7da1653eebed93bcb3ff252f4df9a84dab35
Igor Potemkin <ipotemkin@rambler.ru>
2023-01-08 02:09:57 +0600

    refactor: Profile

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -6,24 +6,13 @@ import { MyGoods } from '../../components/MyGoods/MyGoods'
 import { PageTitle } from '../../components/PageTitle/PageTitle'
 import { Page } from '../Page/Page'
 
-// import styles from './style.module.css'
-
 export const Profile = () => {
   const { data: user, isLoading, error } = useGetUserQuery(0, { refetchOnMountOrArgChange: true })
   const header = `Здравствуйте, ${user?.name || user?.email}!`
   
-  // useEffect(() => {
-  //   refetch()
-  // }, [])
   if (isLoading && !error) return <h2>Загрузка...</h2>
 
   if (error) return <h2>{JSON.stringify(error)}</h2>
-  // {
-  //   if ('status' in error && error.status === 401) {
-  //     dispatch(deleteTokens())
-  //     navigate(ROUTES.login)
-  //   } else return <h2>{JSON.stringify(error)}</h2>
-  // }
   
   return (
     <Page mode="subheader">
